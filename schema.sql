67ea56de366b5035dbab374f62f051b6064f67c3
itsnotm3h <thewhit3pages@gmail.com>
2025-01-15 11:05:52 +0800

    2.1 - updated

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ USE db_dreamvault
 CREATE TABLE products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
+    price DECIMAL(10,2) NOT NULL, 
     image VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
     series VARCHAR(255) NOT NULL
